b96afab48364f70cba4f35911a3a184be5da1123
Wenjia Luo <luowenjia1998@gmail.com>
2023-08-28 23:03:03 +0200

    Initial commit

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -7,11 +7,11 @@ $(".controls").css("visibility", "visible");
 
 const instructions = [
     "Welcome! The 8 BIT SEQUENCER is a music app that allows you to create your own 8-bit-style loops.<br> <br>Press Next to continue through the instructions, or press Skip to head straight to playing!",
-    "The grid of buttons represents a series of sounds that will be played in a loop. Each color corresponds to a different instrument.",
-    "When the sequencer is playing, it will highlight which sounds are being played at that moment in your loop.",
-    "The main controls are on the left. The triangle at the top is the play button .<br> <br>The slider adjusts the speed at which your loop will play.",
-    "The randomize button will randomly change the speed and all the buttons on the grid. The clear button will clear the entire grids.",
-    "The example sequence might give you a better idea of how it works. Try playing it after you click Finish!"
+    "The grid of buttons represents a series of sounds that will be played in a loop.<br> <br>Each color corresponds to a different instrument.",
+    "When the sequencer is playing, it will highlight and play the sounds that are turned on in your loop.",
+    "The main controls are on the left.<br> <br>The triangle at the top is the play button.<br> <br>The slider adjusts the speed at which your loop will play.",
+    "The randomize button will randomly change the speed and all the buttons on the grids.<br> <br>The clear button will clear the entire grids.",
+    "The example sequence might give you a better idea of how it works.<br> <br>Try playing it after you click Finish!"
 ];
 
 $(".instruction").html(instructions[0]);
